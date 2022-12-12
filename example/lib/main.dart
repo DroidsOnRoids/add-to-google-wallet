9b32b9f580143e2da8155d5f1181067812d3347a
jakubjaniec <jakubjaniec@aol.com>
2022-12-12 12:29:55 +0100

    Add multi language support and document fields inside AddToWalletButton

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:add_to_google_wallet/button_language.dart';
 import 'package:add_to_google_wallet/widgets/add_to_google_wallet_button.dart';
 import 'package:flutter/material.dart';
+
+final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
 
 void main() => runApp(const MyApp());
 
@@ -8,18 +11,119 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Plugin example app'),
-          ),
-          body: Center(
-            child: AddToGoogleWalletButton(
-              pass: '',
-              onError: (e) {},
-              onSuccess: () {},
-              onCanceled: () {},
+        home: ScaffoldMessenger(
+          key: scaffoldMessengerKey,
+          child: Scaffold(
+            appBar: AppBar(
+              title: const Text('Plugin example app'),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AddToGoogleWalletButton(
+                    pass: _examplePass,
+                    onError: (Object error) => _onError(context, error),
+                    onSuccess: () => _onSuccess(context),
+                    onCanceled: () => _onCanceled(context),
+                  ),
+                  const SizedBox(height: 8.0),
+                  AddToGoogleWalletButton(
+                    pass: _examplePass,
+                    onError: (Object error) => _onError(context, error),
+                    onSuccess: () => _onSuccess(context),
+                    onCanceled: () => _onCanceled(context),
+                    language: ButtonLanguage.pl,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       );
+
+  void _onError(BuildContext context, Object error) => scaffoldMessengerKey.currentState?.showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.red,
+          content: Text(error.toString()),
+        ),
+      );
+
+  void _onSuccess(BuildContext context) => scaffoldMessengerKey.currentState?.showSnackBar(
+        const SnackBar(
+          backgroundColor: Colors.green,
+          content: Text('Pass has been successfully added to the Google Wallet.'),
+        ),
+      );
+
+  void _onCanceled(BuildContext context) => scaffoldMessengerKey.currentState?.showSnackBar(
+        const SnackBar(
+          backgroundColor: Colors.yellow,
+          content: Text('Adding a pass has been canceled.'),
+        ),
+      );
 }
+
+final String _examplePass = """
+    {
+      "iss": "example@example.com",
+      "aud": "google",
+      "typ": "savetowallet",
+      "iat": ${DateTime.now().millisecondsSinceEpoch},
+      "origins": [],
+      "payload": {
+        "genericObjects": [
+          {
+            "id": "3378000000012184345.89a231pl-4477-9d33-8b44-9088mpp3122g0",
+            "classId": "3378000000012184345.36b814de-4774-4a12-8a1d-4211d88daac9",
+            "genericType": "GENERIC_TYPE_UNSPECIFIED",
+            "hexBackgroundColor": "#4285f4",
+            "logo": {
+              "sourceUri": {
+                "uri": "https://storage.googleapis.com/wallet-lab-tools-codelab-artifacts-public/pass_google_logo.jpg"
+              }
+            },
+            "cardTitle": {
+              "defaultValue": {
+                "language": "en",
+                "value": "Google I/O '22  [DEMO ONLY]"
+              }
+            },
+            "subheader": {
+              "defaultValue": {
+                "language": "en",
+                "value": "Attendee"
+              }
+            },
+            "header": {
+              "defaultValue": {
+                "language": "en",
+                "value": "Alex McJacobs"
+              }
+            },
+            "barcode": {
+              "type": "QR_CODE",
+              "value": "4444"
+            },
+            "heroImage": {
+              "sourceUri": {
+                "uri": "https://storage.googleapis.com/wallet-lab-tools-codelab-artifacts-public/google-io-hero-demo-only.jpg"
+              }
+            },
+            "textModulesData": [
+              {
+                "header": "POINTS",
+                "body": "1234",
+                "id": "points"
+              },
+              {
+                "header": "CONTACTS",
+                "body": "33",
+                "id": "contacts"
+              }
+            ]
+          }
+        ]
+      }
+    }
+""";
--- lib/widgets/add_to_google_wallet_button.dart
+++ lib/widgets/add_to_google_wallet_button.dart
@@ -1,10 +1,25 @@
 import 'package:add_to_google_wallet/add_to_google_wallet.dart';
+import 'package:add_to_google_wallet/button_language.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+const double _buttonRadius = 24.5;
 
 class AddToGoogleWalletButton extends StatelessWidget {
+  /// [Generic pass JSON Object](https://developers.google.com/wallet/generic/android#1_create_a_passes_object)
+  /// [Google codelab example](https://codelabs.developers.google.com/add-to-wallet-android#5)
   final String pass;
+
+  /// Language of the Add To Google Wallet button
+  final ButtonLanguage language;
+
+  /// Called when an error was thrown while adding a pass to the Google Wallet
   final Function(Object)? onError;
+
+  /// Called when a pass was successfully added to the Google Wallet
   final VoidCallback? onSuccess;
+
+  /// Called when a user canceled process of adding a pass to the Google Wallet
   final VoidCallback? onCanceled;
 
   const AddToGoogleWalletButton({
@@ -12,14 +27,24 @@ class AddToGoogleWalletButton extends StatelessWidget {
     this.onError,
     this.onSuccess,
     this.onCanceled,
+    this.language = ButtonLanguage.en,
     required this.pass,
   });
 
   @override
-  Widget build(BuildContext context) => ElevatedButton(
-        onPressed: _onAddToGoogleWalletTap,
-        //Official SVGs (https://developers.google.com/wallet/legacy-resources/button-guidelines) with multi-language support to be added
-        child: const Text('Add to google wallet'),
+  Widget build(BuildContext context) => Stack(
+        children: [
+          SvgPicture.asset(language.path),
+          Positioned.fill(
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: _onAddToGoogleWalletTap,
+                borderRadius: const BorderRadius.all(Radius.circular(_buttonRadius)),
+              ),
+            ),
+          )
+        ],
       );
 
   void _onAddToGoogleWalletTap() => AddToGoogleWallet().saveLoyaltyPass(
